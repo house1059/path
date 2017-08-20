ed5563d48989e744ab90828c989f525929aef73f
koki <house1059@gmail.com>
2017-08-21 01:00:18 +0900

    closedxmlの実装を開始

---
+++ WindowsFormsApp1/xlsPath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace WindowsFormsApp1
+{
+    /// <summary>
+    /// xlsからPathLink.txtを作成する。ClosedXmlを使用
+    /// </summary>
+    class xlsPath
+    {
+        XLWorkbook workbook = new XLWorkbook();
+
+
+    }
+}
